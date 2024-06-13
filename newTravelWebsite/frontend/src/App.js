4696922e8643903242463f865132e57e8a17c8b5
Michael Li <70918897+hypersad@users.noreply.github.com>
2024-06-12 20:10:33 -0700

    Add Review

--- newTravelWebsite/frontend/src/App.js
+++ newTravelWebsite/frontend/src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Route,Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Home from './pages/Home';
 import Fortest from './pages/Fortest';
 import PlaceDetails from './pages/PlaceDetails';
 import Pic from './pages/Pic';
-
-
+import Review from './pages/Review';
 
 function App() {
   return (
     <Router>
       <div className="App">
-    
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/test" element={<Fortest />} />
-          <Route path="/PlaceDetails" element={<PlaceDetails />}/>
-          <Route path="/Pic" element={<Pic />}/>
+          <Route path="/PlaceDetails" element={<PlaceDetails />} />
+          <Route path="/Pic" element={<Pic />} />
+          <Route path="/review" element={<Review />} />
         </Routes>
       </div>
     </Router>
