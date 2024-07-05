9c2754daddaae9150e02eb26090babec309feb54
Zhuocheng Li <70918897+hypersad@users.noreply.github.com>
2024-07-05 12:28:46 -0700

    Add API endpoints for creating and retrieving attractions and reviews
    This update has been tested with Postman. Refer to the documentation for details. The next update will include integration with the front-end and User Authorization. Attraction.js is a temporary file that will be merged with Bin Li's PageDetail.js in the future.

--- newTravelWebsite/backend-2/routes/attraction.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const { addAttraction, getAttraction } = require("../controllers/attraction");
-
-router.post("/add", addAttraction);
-router.get("/:gmap_id", getAttraction);
-
-module.exports = router;
-
