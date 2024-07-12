7a3b6f8649afb73f0a2cbeeac6e94891e591b9f7
Li <xinglisds@gmail.com>
2024-07-12 00:33:29 -0700

    create a new datamodel for comment and fix post and user model

--- TravelWebsite/backend/routes/posts.js
+++ TravelWebsite/backend/routes/posts.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const { createPost, getAllPost } = require('../controllers/postController')
+const { createPost, getAllPost, getPostItem, createComment } = require('../controllers/postController')
 const upload = require('../middlewares/upload');
 const router = express.Router();
 
 router.post('/', upload, createPost)
 router.get('/all', getAllPost)
+router.get('/:id', getPostItem)
+router.post('/writecomment/:id', createComment)
+
 
 module.exports = router
