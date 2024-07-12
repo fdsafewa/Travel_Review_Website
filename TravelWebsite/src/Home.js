7c44674988307f22c54d90c6c3d8877421cf129e
Codingsuixiang <160776897+Codingsuixiang@users.noreply.github.com>
2024-07-12 00:07:53 -0700

    Update Home.js

--- TravelWebsite/src/Home.js
+++ TravelWebsite/src/Home.js
@@ -1,17 +1,21 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
-
-import AddPlace from "./pages/AddPlace"
-
-
+import EditPlace from "./pages/EditPlace";
+import AddPlace from "./pages/AddPlace";
 
 const Home = () => {
     return (
-        <div className="homePage">
-            <AddPlace />
-        </div>
-    )
-}
+
+      <div className="App">
+         <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AddPlace />} />
+          <Route path="/editplace" element={<EditPlace />} />
+        </Routes>
+        </BrowserRouter>
+      </div>
+    );
+  };
 
 export default Home;
